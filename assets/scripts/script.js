f027aad8815e1d38c319ed4556b58a231f65f1de
Shanel Beebe <shabeebe1@WKMZTFD72BE9.global.publicisgroupe.net>
2023-06-02 08:45:49 -0600

    fixed issue where design section click event was triggering focusout event as well.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,6 +3,7 @@ const menuIcon = document.querySelector(".menu-icon");
 const navBar = document.querySelector(".header-nav");
 const designImages = document.querySelector(".design-img");
 const designClicked = document.querySelectorAll(".design-img img");
+console.log(designClicked )
 
 // toggles the nav menu open and closed when menu icon is clicked
 menuIcon.addEventListener("click", () => {
@@ -20,9 +21,26 @@ navBar.addEventListener("click", () => {
   menuIcon.innerHTML = '<i class="fa-solid fa-bars"></i>';
 });
 // toggles class to zoom clicked image
-designClicked.forEach((img) => {
-  img.addEventListener("click", function () {
-    designImages.classList.toggle("pos-rel");
-    this.classList.toggle("design-click");
-  });
-});
+
+
+function clickHandler (e) {
+  e.stopPropagation()
+  designImages.classList.toggle("pos-rel");
+  e.target.classList.toggle("design-click");
+}
+
+designClicked.forEach(img => {
+  img.addEventListener('mousedown', clickHandler)
+  img.addEventListener('focusin', function() {
+    designImages.classList.add("pos-rel");
+    img.classList.add("design-click");
+    console.log('focusin')
+  })
+  img.addEventListener("focusout", function(e) {
+    designImages.classList.remove("pos-rel");
+    img.classList.remove("design-click");
+    console.log('focusout')
+    })
+})
+
+
