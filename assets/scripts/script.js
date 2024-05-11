3c2adb284db3398594f567dd9cbb75c240d1b220
Shanel Beebe <shanel.beebe@publicisna.com>
2024-05-11 14:13:08 -0600

    update logo and links to reflect my new last name. update css for transition states.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,8 +2,9 @@
 const menuIcon = document.querySelector(".menu-icon");
 const navBar = document.querySelector(".header-nav");
 const designImages = document.querySelector(".design-img");
-const designClicked = document.querySelectorAll(".design-img img");
-console.log(designClicked )
+const designClicked = document.querySelectorAll(".design-item");
+const projects = document.querySelectorAll('.project');
+const projectImg = document.querySelectorAll('.project-img');
 
 // toggles the nav menu open and closed when menu icon is clicked
 menuIcon.addEventListener("click", () => {
@@ -15,14 +16,14 @@ menuIcon.addEventListener("click", () => {
     menuIcon.innerHTML = '<i class="fa-solid fa-bars"></i>';
   }
 });
+
 // toggles menu closed when a link is clicked
 navBar.addEventListener("click", () => {
   navBar.classList.toggle("open");
   menuIcon.innerHTML = '<i class="fa-solid fa-bars"></i>';
 });
-// toggles class to zoom clicked image
 
-
+// toggles class to zoom clicked image\
 function clickHandler (e) {
   e.stopPropagation()
   designImages.classList.toggle("pos-rel");
@@ -42,5 +43,30 @@ designClicked.forEach(img => {
     console.log('focusout')
     })
 })
+
+
+// expands individual project container when a mouseover happens on the project image.
+projects.forEach(function(project) {
+  project.addEventListener('mouseover', function(e) {
+    if (e.target.className === 'project-img') {
+      project.classList.add("expand");
+    }
+  })
+})
+
+// shrinks individual project container when a mouseout happens on the project image.
+projects.forEach(function(project) {
+  project.addEventListener('mouseout', function(e) {
+    if (e.target.className === 'project-img') {
+      project.classList.remove("expand");
+    }
+  })
+})
+
+
+
+
+
+
 
 
